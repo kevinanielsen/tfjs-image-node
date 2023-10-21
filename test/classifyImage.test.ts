77bf5cd5aa02ed6cf9cce15801e0ae26de24ef15
Kevin Nielsen <keveran@gmail.com>
2023-10-21 11:00:19 +0200

    test: classifyImage function (#7)
    * test: add test for classifyImage
    
    * feat: change export to export default
    
    * test: update npm test script
    
    * chore(deps): add @types/mocha
    
    * test: add assertion for other image
    
    * test: add assertion for probability level

---
+++ test/classifyImage.test.ts
@@ -0,0 +1,30 @@
+import { describe } from "mocha";
+import classifyImage from "../src/index";
+import assert from "assert";
+
+const metadata = require("./testMetadata.json");
+const model = "https://teachablemachine.withgoogle.com/models/jAIOHvmge";
+const imageHand =
+  "https://www.stgeorges.nhs.uk/wp-content/uploads/2014/03/hand-2.jpeg";
+const imageNoHand =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg";
+
+describe("classifyImage function", async () => {
+  it("returns hand when shown a picture of a hand", async () => {
+    const result = await classifyImage(model, imageHand, metadata);
+
+    assert.equal(result[0].label, "Hand");
+  });
+
+  it("returns 'No hand' when shown a picture not including hand", async () => {
+    const result = await classifyImage(model, imageNoHand, metadata);
+
+    assert.equal(result[0].label, "No hand");
+  });
+
+  it("returns a probability level", async () => {
+    const result = await classifyImage(model, imageNoHand, metadata);
+
+    assert.notEqual(result[0].probability, null);
+  });
+});
