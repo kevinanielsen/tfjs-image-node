a614c65b6b1e15dfa7d8f75e56877484dbeca99c
kevinanielsen <keveran@gmail.com>
2023-10-21 10:50:45 +0200

    test: add assertion for other image

--- test/classifyImage.test.ts
+++ test/classifyImage.test.ts
@@ -4,13 +4,20 @@ import assert from "assert";
 
 const metadata = require("./testMetadata.json");
 const model = "https://teachablemachine.withgoogle.com/models/jAIOHvmge";
-const image =
+const imageHand =
   "https://www.stgeorges.nhs.uk/wp-content/uploads/2014/03/hand-2.jpeg";
+const imageNoHand =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg";
 
 describe("classifyImage function", async () => {
   it("returns hand when shown a picture of a hand", async () => {
-    const result = await classifyImage(model, image, metadata);
+    const result = await classifyImage(model, imageHand, metadata);
 
     assert.equal(result[0].label, "Hand");
+  });
+  it("returns 'No hand' when shown a picture not including hand", async () => {
+    const result = await classifyImage(model, imageNoHand, metadata);
+
+    assert.equal(result[0].label, "No hand");
   });
 });
