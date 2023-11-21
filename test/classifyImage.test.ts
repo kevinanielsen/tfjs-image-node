96f2bde80e3a278e688dc080d8adceb49b198759
kevinanielsen <kevinanielsen@outlook.com>
2023-11-21 12:51:55 +0100

    test: add tests for image inputs

--- test/classifyImage.test.ts
+++ test/classifyImage.test.ts
@@ -7,9 +7,11 @@ const imageHand =
   "https://www.stgeorges.nhs.uk/wp-content/uploads/2014/03/hand-2.jpeg";
 const imageNoHand =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg";
+const imageHandJPEG = "./test/images/hand.jpeg";
+const imageHandPNG = "./test/images/hand.png";
 
 describe("classifyImage function", async () => {
-  describe("returns", async () => {
+  describe("Result returns", async () => {
     it("returns hand when shown a picture of a hand", async () => {
       const result = await classifyImage(model, imageHand);
       if (result instanceof Error) {
@@ -44,6 +46,32 @@ describe("classifyImage function", async () => {
       const result = await classifyImage(imageNoHand);
 
       assert.ok(result instanceof Error);
+    });
+  });
+  describe("Image types", async () => {
+    it("returns a result on url image-input", async () => {
+      const result = await classifyImage(model, imageHand);
+      if (result instanceof Error) {
+        return new Error();
+      } else {
+        assert.equal(result[0].label, "Hand");
+      }
+    });
+    it("returns a result on JPEG image-input", async () => {
+      const result = await classifyImage(model, imageHandJPEG);
+      if (result instanceof Error) {
+        return new Error();
+      } else {
+        assert.equal(result[0].label, "Hand");
+      }
+    });
+    it("returns a result on PNG image-input", async () => {
+      const result = await classifyImage(model, imageHandPNG);
+      if (result instanceof Error) {
+        return new Error();
+      } else {
+        assert.equal(result[0].label, "Hand");
+      }
     });
   });
 });
