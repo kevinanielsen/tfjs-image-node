9ba913b0f4397d7cf48c18c2b4f094f7d3e17041
kevinanielsen <keveran@gmail.com>
2023-11-27 21:57:45 +0100

    REFACTOR: splitting code into multiple imports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/js/index.ts", "src/node/index.ts"],
+  entry: ["src/**"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: true,
   clean: true,
+  minify: true,
 });
