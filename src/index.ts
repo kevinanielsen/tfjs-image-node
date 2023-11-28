fe5c99825ddd43f39f4315cb4d51300c99f27d5e
Kevin Nielsen <kevinanielsen@outlook.com>
2023-11-28 20:31:47 +0100

    [Feature] Remove necessity for no / at end of MODEL_DIR_PATH (#18)
    * feat: add playground for trying out the package
    
    * chore: add playground to .npmignore
    
    * chore: remove playground
    
    * feat: set up filter to remove '/' at end if it exists
    
    * test: add test for model_dir_path with/without slash at end
    
    * chore: changeset
    
    * docs: update readme
    
    * chore: format

--- src/index.ts
+++ src/index.ts
@@ -4,90 +4,89 @@ const tfJs = require("@tensorflow/tfjs");
 let tf: any;
 
 interface IMetadata extends JSON {
-  labels: string[];
+	labels: string[];
 }
 
 type ResultType = {
-  label: string;
-  probability: string;
+	label: string;
+	probability: string;
 };
 
 type ClassifyImageType = (
-  MODEL_DIR_PATH: string,
-  IMAGE_FILE_PATH: string,
-  PLATFORM?: "node" | "classic",
+	MODEL_DIR_PATH: string,
+	IMAGE_FILE_PATH: string,
+	PLATFORM?: "node" | "classic"
 ) => Promise<ResultType[] | Error>;
 
+const filterInputPath = (inputPath: string) => {
+	if (inputPath.endsWith("/")) {
+		return inputPath.slice(0, -1);
+	}
+	return inputPath;
+};
+
 const classifyImage: ClassifyImageType = async (
-  MODEL_DIR_PATH,
-  IMAGE_FILE_PATH,
-  PLATFORM = "node",
+	MODEL_DIR_PATH,
+	IMAGE_FILE_PATH,
+	PLATFORM = "node"
 ) => {
-  PLATFORM === "node" ? (tf = tfNode) : (tf = tfJs);
+	PLATFORM === "node" ? (tf = tfNode) : (tf = tfJs);
 
-  if (!MODEL_DIR_PATH || !IMAGE_FILE_PATH) {
-    return new Error("MISSING_PARAMETER");
-  }
+	if (!MODEL_DIR_PATH || !IMAGE_FILE_PATH) {
+		return new Error("MISSING_PARAMETER");
+	}
 
-  const res = await fetch(`${MODEL_DIR_PATH}/metadata.json`);
-  if (res.status !== 200) {
-    return new Error("METADATA_NOT_FOUND");
-  }
+	MODEL_DIR_PATH = filterInputPath(MODEL_DIR_PATH);
 
-  const METADATA: IMetadata = await res.json();
+	const res = await fetch(`${MODEL_DIR_PATH}/metadata.json`);
+	if (res.status !== 200) {
+		return new Error("METADATA_NOT_FOUND");
+	}
 
-  if (METADATA["labels"].length === 0 || METADATA["labels"]! instanceof Array) {
-    return new Error("NO_METADATA_LABELS");
-  }
+	const METADATA: IMetadata = await res.json();
 
-  let labels: string[] = METADATA["labels"];
+	if (METADATA["labels"].length === 0 || METADATA["labels"]! instanceof Array) {
+		return new Error("NO_METADATA_LABELS");
+	}
 
-  const model = await tf.loadLayersModel(`${MODEL_DIR_PATH}/model.json`);
+	let labels: string[] = METADATA["labels"];
 
-  const image = await Jimp.read(IMAGE_FILE_PATH);
-  image.cover(
-    224,
-    224,
-    Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE,
-  );
+	const model = await tf.loadLayersModel(`${MODEL_DIR_PATH}/model.json`);
 
-  const NUM_OF_CHANNELS = 3;
-  let values = new Float32Array(224 * 224 * NUM_OF_CHANNELS);
+	const image = await Jimp.read(IMAGE_FILE_PATH);
+	image.cover(224, 224, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
 
-  let i = 0;
-  image.scan(
-    0,
-    0,
-    image.bitmap.width,
-    image.bitmap.height,
-    (x: number, y: number) => {
-      const pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
-      pixel.r = pixel.r / 127.0 - 1;
-      pixel.g = pixel.g / 127.0 - 1;
-      pixel.b = pixel.b / 127.0 - 1;
-      pixel.a = pixel.a / 127.0 - 1;
-      values[i * NUM_OF_CHANNELS + 0] = pixel.r;
-      values[i * NUM_OF_CHANNELS + 1] = pixel.g;
-      values[i * NUM_OF_CHANNELS + 2] = pixel.b;
-      i++;
-    },
-  );
+	const NUM_OF_CHANNELS = 3;
+	let values = new Float32Array(224 * 224 * NUM_OF_CHANNELS);
 
-  const outShape = [224, 224, NUM_OF_CHANNELS];
-  let img_tensor = tf.tensor3d(values, outShape, "float32");
-  img_tensor = img_tensor.expandDims(0);
+	let i = 0;
+	image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x: number, y: number) => {
+		const pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
+		pixel.r = pixel.r / 127.0 - 1;
+		pixel.g = pixel.g / 127.0 - 1;
+		pixel.b = pixel.b / 127.0 - 1;
+		pixel.a = pixel.a / 127.0 - 1;
+		values[i * NUM_OF_CHANNELS + 0] = pixel.r;
+		values[i * NUM_OF_CHANNELS + 1] = pixel.g;
+		values[i * NUM_OF_CHANNELS + 2] = pixel.b;
+		i++;
+	});
 
-  const predictions = await model.predict(img_tensor).dataSync();
+	const outShape = [224, 224, NUM_OF_CHANNELS];
+	let img_tensor = tf.tensor3d(values, outShape, "float32");
+	img_tensor = img_tensor.expandDims(0);
 
-  let result: ResultType[] = [];
+	const predictions = await model.predict(img_tensor).dataSync();
 
-  for (let i = 0; i < predictions.length; i++) {
-    const label = labels[i];
-    const probability = predictions[i];
-    result.push({ label: label, probability: probability });
-  }
+	let result: ResultType[] = [];
 
-  return result.sort((a, b) => Number(b.probability) - Number(a.probability));
+	for (let i = 0; i < predictions.length; i++) {
+		const label = labels[i];
+		const probability = predictions[i];
+		result.push({ label: label, probability: probability });
+	}
+
+	return result.sort((a, b) => Number(b.probability) - Number(a.probability));
 };
 
 export default classifyImage;
